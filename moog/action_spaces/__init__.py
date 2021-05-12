56381942b221deb51fe6468d298a576e17d62acf
“juanpablordz” <juanpablordz94@gmail.com>
2021-05-12 10:40:10 -0400

    Add grid variation actions spaces

--- moog/action_spaces/__init__.py
+++ moog/action_spaces/__init__.py
@@ -3,5 +3,7 @@
 from .abstract_action_space import AbstractActionSpace
 from .composite import Composite
 from .grid import Grid
+from .vertical_grid import VerticalGrid
+from .horizontal_grid import HorizontalGrid
 from .joystick import Joystick
 from .set_position import SetPosition
